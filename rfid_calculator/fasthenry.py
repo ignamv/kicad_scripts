7f6387ae6277135785c8abf98bc1a6f042a8b201
Ignacio Martinez Vazquez <ignamv@gmail.com>
2015-11-01 16:57:30 -0300

    Add InputFile class for building Fasthenry input files

--- rfid_calculator/fasthenry.py
+++ rfid_calculator/fasthenry.py
@@ -1,5 +1,63 @@
 import subprocess
 import os
+import sys
+import io
+
+class Fasthenry(object):
+
+    class MissingNodeException(Exception):
+        pass
+
+class InputFile(io.StringIO):
+    def __init__(self):
+        super().__init__()
+        self.nodes = []
+        self.elements = 0
+        self.write('*\n')
+    @staticmethod
+    def node(name, x, y, z):
+        return name + ' x={} y={} z={}'.format(x, y, z)
+
+    @staticmethod
+    def element(name, node1, node2, width, height, 
+            width_filaments, height_filaments):
+        return name + ' ' + node1 + ' ' + node2 + \
+                ' w={} h={} nwinc={} nhinc={}'.format(width, height,
+                        width_filaments, height_filaments)
+
+    @staticmethod
+    def external(name, node1, node2):
+        return ' '.join(['.external', node1, node2, name])
+
+    @staticmethod
+    def sweep(min_frequency, max_frequency, points_per_decade):
+        return '.freq fmin={} fmax={} ndec={}'.format(min_frequency,
+                max_frequency, points_per_decade)
+
+    def addnode(self, x, y, z):
+        name = 'n{}'.format(len(self.nodes))
+        self.write(self.node(name, x, y, z))
+        self.nodes.append(name)
+        return name
+
+    def addelement(self, node1, node2, width, height, width_filaments,
+            height_filaments):
+        if node1 not in self.nodes or node2 not in self.nodes:
+            raise Fasthenry.MissingNodeException()
+        name = 'e{}'.format(self.elements)
+        self.write(self.element(name, node1, node2, width, height,
+            width_filaments, height_filaments))
+        self.elements += 1
+        return name
+
+    def addport(self, name, node1, node2):
+        if node1 not in self.nodes or node2 not in self.nodes:
+            raise Fasthenry.MissingNodeException()
+        self.write(self.external(name, node1, node2))
+
+    #def addcoil(self, name, vertices, width, height, width_filaments,
+            #height_filaments
+
 
 def coil_impedance(vertices, trace_width, trace_height, frequency):
     process = subprocess.Popen(['./fasthenry'],
